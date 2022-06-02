716e9fbe82961f47c00bf5d51b0a0fb7b2cc42f4
Alexis Firome <alexis.firome@epfl.ch>
2022-06-02 23:24:49 +0200

    end menu

---
+++ Assets/Scripts/Menu/FirstPage.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPage : MonoBehaviour
+{
+   /* public Animator transition;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            LoadNextScene();
+        }
+        
+    }
+
+    public void LoadNextScene()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+
+    }
+
+    IEnumerator LoadLevel(int sceneindex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(10);
+
+        SceneManager.LoadScene(sceneindex);
+    }*/
+}
