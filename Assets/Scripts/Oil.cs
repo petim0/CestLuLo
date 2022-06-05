a755d9996b61084df98247f2597d344ff084a2ca
Arun Navaranjan <arun.navaranjan@epfl.ch>
2022-06-05 21:45:37 +0200

    made Oil+inverted controls added

---
+++ Assets/Scripts/Oil.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oil : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other) {
+        string tag = other.transform.parent.gameObject.tag;
+        if (tag == "Player1" || tag == "Player2") {
+            other.transform.parent.gameObject.GetComponent<PlayerEffectsManager>().Slide();
+            //other.transform.parent.gameObject.GetComponent<MoveWithKeyboardBehavior>().InverseControl();
+        } else {
+        }
+    }
+
+
+    public void OnTriggerExit(Collider other) {
+        string tag = other.transform.parent.gameObject.tag;
+        if (tag == "Player1" || tag == "Player2") {
+            other.transform.parent.gameObject.GetComponent<PlayerEffectsManager>().StopSliding();
+        } else {
+        }
+    }
+}
