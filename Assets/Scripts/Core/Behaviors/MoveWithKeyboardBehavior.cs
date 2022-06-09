3786a575679aa1f9d8ba8cdf46ffa54c278fc7d7
Timo Achard <timo@apolline.eu>
2022-06-09 10:37:16 +0200

    made some small modification

--- Assets/Scripts/Core/Behaviors/MoveWithKeyboardBehavior.cs
+++ Assets/Scripts/Core/Behaviors/MoveWithKeyboardBehavior.cs
@@ -77,7 +77,7 @@ public class MoveWithKeyboardBehavior : AgentBehaviour
         {
 
             float speed = 0;
-            float brakingSpeed;
+            float brakingSpeed = 0;
             bool isGoingForward = lastspeed >= 0 ;
 
             if (isGoingForward)
@@ -89,8 +89,9 @@ public class MoveWithKeyboardBehavior : AgentBehaviour
             else {
                 //Debug.Log((Input.GetAxis("Vertical") * agent.maxAccel).ToString() + " " + (lastspeed + (float)0.1).ToString());
                 //Debug.Log(Math.Min(Input.GetAxis("Vertical") * agent.maxAccel, lastspeed + (float)0.1).ToString());
-                speed = Math.Min(Input.GetAxis("Vertical") * agent.maxAccel, lastspeed + (float)0.1);
-                brakingSpeed = -BRAKING;
+
+                //Pas d'inertie en arrière, c'est pas cool pour jouer
+                speed = Input.GetAxis("Vertical") * agent.maxAccel;
 
             }
 
@@ -140,97 +141,76 @@ public class MoveWithKeyboardBehavior : AgentBehaviour
         }
         else if ((int) inputKeyboard == 1)
         {
-            //float speed = Input.GetAxis("VerticalWASD") * agent.maxAccel;
-            //PARALYZED
-            float speed;
-            if (isParalyzed && paralyzedTime > 0) {
-                paralyzedTime -= 1;
-                speed = 0;
-                Debug.Log("Timer 2: "+paralyzedTime);
-                if (paralyzedTime < 0) {
-                    isParalyzed = false;
-                }
-            } else {
-                speed = Input.GetAxis("VerticalWASD") * agent.maxAccel;
+            float speed = 0;
+            float brakingSpeed = 0;
+            bool isGoingForward = lastspeed >= 0;
+
+            if (isGoingForward)
+            {
+                speed = Math.Max(Input.GetAxis("VerticalWASD") * agent.maxAccel, lastspeed - (float)0.1);
+                brakingSpeed = BRAKING;
+
             }
-            
-            float rotation;
-            int divider = (isSliding && !isParalyzed) ? slidingRotationDivider : rotationDivider;
-            if (isInverted && invertedTime > 0) {
-                invertedTime -= 1;
-                rotation = Input.GetAxis("HorizontalWASD") / divider;
-                if (invertedTime < 0) {
-                    isInverted = false;
-                }
-            } else {
-                rotation = - Input.GetAxis("HorizontalWASD") / divider;
+            else
+            {
+                //Debug.Log((Input.GetAxis("Vertical") * agent.maxAccel).ToString() + " " + (lastspeed + (float)0.1).ToString());
+                //Debug.Log(Math.Min(Input.GetAxis("Vertical") * agent.maxAccel, lastspeed + (float)0.1).ToString());
+
+                //Pas d'inertie en arrière, c'est pas cool pour jouer
+                speed = Input.GetAxis("VerticalWASD") * agent.maxAccel;
+
             }
 
+
+            if (Input.GetAxis("breakWASD") > 0)
+            {
+                speed = lastspeed - brakingSpeed;
+                if ((speed < 0 && isGoingForward) || (speed > 0 && !isGoingForward))
+                {
+                    speed = 0;
+                }
+            }
+
+            if (isParalyzed && paralyzedTime > 0)
+            {
+                paralyzedTime -= 1;
+                speed = 0;
+                Debug.Log("Timer 2: " + paralyzedTime);
+                if (paralyzedTime < 0)
+                {
+                    isParalyzed = false;
+                }
+            }
+
+
+            lastspeed = speed;
+
+
+            float rotation;
+            int divider = (isSliding && !isParalyzed) ? slidingRotationDivider : rotationDivider;
+            if (isInverted && invertedTime > 0)
+            {
+                invertedTime -= 1;
+                rotation = Input.GetAxis("HorizontalWASD") / divider;
+                if (invertedTime < 0)
+                {
+                    isInverted = false;
+                }
+            }
+            else
+            {
+                rotation = -Input.GetAxis("HorizontalWASD") / divider;
+            }
 
             if (speed != 0)
             {
                 dir = RotateVector2d(dir, rotation).normalized;
             }
-            //ADD INERTIA !!!
+
             this.transform.LookAt(this.transform.position + dir);
             steering.linear = dir * speed;
 
             steering.linear = this.transform.parent.TransformDirection(Vector3.ClampMagnitude(steering.linear, agent.maxAccel));
-        }
-        else {
-
-            if (Input.GetMouseButton(0))
-            {
-                Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.transform.position.y));
-
-                if (!mooving)
-                {
-                    RaycastHit hit;
-
-                    pos.y = Camera.transform.position.y;
-                    //Debug.Log("Mouse pos to WORLD: " + pos.ToString());
-                    //Debug.Log("Player pos: " + gameObject.transform.position.ToString());
-
-                    if (Physics.Raycast(pos, Vector3.down, out hit))
-                    {
-                        //Debug.Log("Found an object - nature: " + hit.collider.transform.parent.tag.ToString());
-                        if (hit.collider.transform.parent.tag.CompareTo(gameObject.transform.tag) == 0)
-                        {
-                            mooving = true;
-                        }
-                        
-                    }
-                }
-                else {
-                    pos.y = gameObject.transform.position.y;
-
-                    Vector3 direction = pos - gameObject.transform.position;
-                    //Debug.Log(direction.ToString());
-
-                    // Juste pour empecher qu'il fasse des aller retours si on laisse la souris sur le cellulo
-                    if (direction.magnitude < 0.1) {
-                        //Debug.Log("magnitude ils too small");
-                        direction = Vector3.zero;
-                    }
-                    direction.Normalize();
-
-                    if (isParalyzed && paralyzedTime > 0) {
-                        paralyzedTime -= 1;
-                        direction = Vector3.zero;
-                        Debug.Log("PARALYZED");
-                    if (paralyzedTime < 0) {
-                        isParalyzed = false;
-                    }
-                }
-
-                //Debug.Log("D After normalisation: " + direction.ToString());
-                    steering.linear = direction * agent.maxAccel;
-                }                
-            }
-             else if (!Input.GetMouseButton(0))
-             {
-                    mooving = false;
-             }
         }
 
         lastSteering = steering;
